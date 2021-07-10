f59a423aaaf90f24b7798a5f29ca413e029f1fe6
nathan hains <nathanhains14@yahoo.com>
2021-07-09 22:17:37 -0400

    Full functionality// No Modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const NavBar = ({clicked, motion}) => {
 
@@ -17,14 +18,17 @@ const NavBar = ({clicked, motion}) => {
         show: { opacity: 1 }
     }
 
-    return clicked === "no" ? null : ( 
-       <motion.div variants={container} initial="hidden" animate="show" className = "nav-main">
-                <motion.h3 variants={item} className="nav-link">WELCOME</motion.h3>
-                <motion.h3 variants={item} className="nav-link">WELCOME</motion.h3>
-                <motion.h3 variants={item} className="nav-link">WELCOME</motion.h3>
-                <motion.h3 variants={item} className="nav-link">WELCOME</motion.h3>
-        </motion.div>
 
+    return clicked === "no" ? null : ( 
+      
+       <motion.div variants={container} initial="hidden" animate="show" className = "nav-main">
+    
+                <motion.h3 variants={item} ><AnchorLink className="nav-link" style={{textDecoration: 'none'}}href="#welcome">Welcome</AnchorLink></motion.h3>
+                <motion.h3 variants={item}><AnchorLink className="nav-link" style={{textDecoration: 'none'}}href="#my-life">My Life</AnchorLink></motion.h3>
+                <motion.h3 variants={item} ><AnchorLink className="nav-link" style={{textDecoration: 'none'}}href="#my-work">My Work</AnchorLink></motion.h3>
+                <motion.h3 variants={item} ><AnchorLink className="nav-link" style={{textDecoration: 'none'}}href="#contact">Contact</AnchorLink></motion.h3>
+ 
+        </motion.div>
     )
 }
 
